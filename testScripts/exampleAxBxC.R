5a95fc8d8ea7a8839fa2fcff4edee0b3c8d81f63
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2021-08-05 13:43:19 +0200

    Updated ABC testScript for comparison

--- testScripts/exampleAxBxC.R
+++ testScripts/exampleAxBxC.R
@@ -24,9 +24,10 @@ head(MPPobj1$calcIBDres$IBDmatrix)[1:6,1:6]
 # 3. genome scan for multi-QTLs
 MPPobj1a <- selQTLmppOrig(MPPobj1, QTLwindow = 10, threshold = 3,
                           trait.name = "pheno", CIM = TRUE)
-MPPobj$Result$QTLcandidates
+MPPobj1a$Result$QTLcandidates
 
 
+##### Identical analysis using new functions.
 
 crossNames <- c("AxB", "AxC") # cross name(s)
 locFiles <- c("testScripts/data/multipop/AxB.loc",
@@ -37,12 +38,13 @@ poptypes <- c("F4DH", "F4DH") # cross type(s)
 mapFile <- "testScripts/data/multipop/mapfile.map" # map file names
 evaldist <- 5 # distance for IBD calc
 
+## Read phenotypic data.
 quaDat <- lapply(quaFiles, read.table, header = TRUE)
 
-
-
-
+# 2. calculate IBDs and create the data frame
 MPPobj2 <- calcIBDmpp(crossNames, locFiles, quaDat,
                       poptypes, mapFile, evaldist)
-
-
+# 3. genome scan for multi-QTLs
+MPPobj2a <- selQTLmpp(MPPobj2, QTLwindow = 10, threshold = 3,
+                      trait = "pheno", CIM = TRUE)
+MPPobj2a$Result$QTLcandidates
