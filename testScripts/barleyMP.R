1966e7e779ba6e8e5b6c700f4e82df28b272a7cf
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2022-01-07 14:10:16 +0100

    Updated barley testscript using pheno data from package

--- testScripts/barleyMP.R
+++ testScripts/barleyMP.R
@@ -1,18 +1,13 @@
+library(statgenMPP)
+
 genoFile <- system.file("extdata", "BarleyMP_magicReconstruct_Summary.zip",
                         package = "statgenMPP")
 
-pheno <- read.csv(system.file("extdata", "BarleyMP_pheno.csv",
-                              package = "statgenMPP"))
-colnames(pheno)[1] <- "genotype"
-colnames(pheno)[3] <- "cross"
-pheno$cross <- factor(pheno$cross)
-pheno <- pheno[, 1:3]
-
-
 barleyMPMPP <- readRABBIT(infile = unzip(genoFile, exdir = tempdir()),
-                          pheno = pheno)
+                          pheno = barleyPheno)
 
-barleyRes <- selQTLmpp(MPPobj = barleyMPMPP, trait = "Awn_length")
+barleyRes <- selQTLmpp(MPPobj = barleyMPMPP, trait = "Awn_length",
+                       maxCofactors = 1)
 
 plot(barleyRes, plotType = "manhattan")
 plot(barleyRes, plotType = "parEffs")
