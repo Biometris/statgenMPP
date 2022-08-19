14572087ed046d72e4b706b67159f166b8c5f419
Martin Boer <martin.boer@wur.nl>
2022-08-19 10:55:48 +0200

    Some extra code to solve problem issue #39

--- testScripts/kinship/testKinship.R
+++ testScripts/kinship/testKinship.R
@@ -28,10 +28,11 @@ ABCMPP <- calcIBDMPP(crossNames = c("AxB", "AxC"),
 doParallel::registerDoParallel(cores = 4)
 
 ## Run multi-QTL mapping.
-ABC_MQM <- selQTLMPP(ABCMPP, trait = "pheno", parallel = TRUE)
+ABC_MQM <- selQTLMPP(ABCMPP, trait = "pheno", parallel = TRUE, maxCofactors = 0)
 
 ## Run multi-QTL mapping with kinship.
-ABC_MQM2 <- selQTLMPP(ABCMPP, trait = "pheno", parallel = TRUE, computeKin = TRUE)
+ABC_MQM2 <- selQTLMPP(ABCMPP, trait = "pheno", parallel = TRUE, computeKin = TRUE,
+                      maxCofactors = 0)
 
 ABC_MQM$signSnp
 ABC_MQM2$signSnp
@@ -39,4 +40,31 @@ ABC_MQM2$signSnp
 plot(ABC_MQM, plotType = "QTLProfileExt")
 plot(ABC_MQM2, plotType = "QTLProfileExt")
 
+# Example how to implement kinship using LMMsolver
 
+library(LMMsolver)
+eps <- 1.0e-10
+chr <- 5
+map <- ABCMPP$map
+markers <- ABCMPP$markers
+
+mrkNamesNonChr <- rownames(map)[map$chr != chr]
+mrkNonChr <- apply(markers[, mrkNamesNonChr, ], MARGIN = 2,
+                   FUN = tcrossprod, simplify = FALSE)
+KChr <- Reduce(`+`, mrkNonChr) / length(mrkNonChr)
+eigKChr <- eigen(KChr)
+
+sel <- eigKChr$values > eps
+U <- eigKChr$vectors[, sel]
+d <- eigKChr$values[sel]
+Usc <- U %*% diag(sqrt(d))
+# Should be close to original kinship
+range(KChr - tcrossprod(Usc))
+
+colnames(Usc) <- paste0("eig", 1:ncol(Usc))
+y <- ABCMPP$pheno$pheno$pheno
+dat <- data.frame(y, Usc)
+
+lGrp <- list(Eigen=2:ncol(dat))
+obj <- LMMsolve(y~1, random=~grp(Eigen), group=lGrp, data=dat)
+summary(obj)
